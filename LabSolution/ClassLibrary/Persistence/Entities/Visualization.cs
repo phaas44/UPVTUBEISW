c0158e05ec8d3bfc44f9eb3617f6f01397050ef2
Patrick Haas <PHAAS@upv.edu.es>
2023-10-05 18:01:15 +0200

    Implementation of the attributes of Visualization.

--- LabSolution/ClassLibrary/Persistence/Entities/Visualization.cs
+++ LabSolution/ClassLibrary/Persistence/Entities/Visualization.cs
@@ -8,5 +8,13 @@ namespace UPVTube.Entities
 {
     public partial class Visualization
     {
+        public ICollection<Member> Members{ get; set; }
+        public ICollection<Content> Contents{ get; set; }
+
+        public int Id { get; set; }
+
+        public string Text{ get; set; }
+
+        public DateTime VisualizationDate { get; set; }
     }
 }
