0cf8623248d7ebbb3be78c7a98f37b6f77ec6567
Patrick Haas <PHAAS@upv.edu.es>
2023-10-05 19:05:52 +0200

    Implementation of the constructors of Visualization class.

--- LabSolution/ClassLibrary/BusinessLogic/Entities/Visualization.cs
+++ LabSolution/ClassLibrary/BusinessLogic/Entities/Visualization.cs
@@ -8,5 +8,22 @@ namespace UPVTube.Entities
 {
     public partial class Visualization
     {
+        public Visualization() {
+
+            Contents = new List<Content>();
+            Members = new List<Member>();
+            
+
+                   
+        
+        }  
+
+        public Visualization(int Id, DateTime VisualzationDate) : this()
+        {
+            this.Id = Id;
+            this.VisualizationDate = VisualizationDate;
+
+
+        }
     }
 }
