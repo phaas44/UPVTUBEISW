d4c7f500f834b89fb6ba26eefb5d9e8f6428b51d
w4lidx <walid.mohoub@gmail.com>
2023-10-11 10:56:03 +0200

    changes

--- LabSolution/ClassLibrary/BusinessLogic/Entities/Evaluation.cs
+++ LabSolution/ClassLibrary/BusinessLogic/Entities/Evaluation.cs
@@ -8,5 +8,14 @@ namespace UPVTube.Entities
 {
     public partial class Evaluation
     {
+        public Evaluation() { }
+
+        public Evaluation(DateTime e, string r, Content c, Member m) : this()
+        {
+            EvaluationDate = e;
+            RejectionReason = r;
+            content = c;
+            member = m;
+        }
     }
 }
