a6cd871a07dbd7ef472b6145c265b2cd3e320478
NotPowah <elrandomgamer@gmail.com>
2023-10-12 18:07:09 +0200

    Domains class added and Content class final

---
+++ LabSolution/ClassLibrary/BusinessLogic/Entities/Domains.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace UPVTube.Entities
+{
+    // This class simulates the existence of a Directory Service
+    // to know whether an Email belongs to a student or lecturer
+
+    public class Domains
+    {
+        private static List<String> TeacherDomains = new List<string> { "@dsic.upv.es", "@dsica.upv.es" };
+        private static List<String> StudentDomains = new List<string> { "@inf.upv.es" };
+        /// <summary>
+		/// Returns true if Email has a lecturer domain
+		/// </summary>
+		/// <returns></returns>
+		public static bool IsTeacherDomain(string Email)
+        {
+            foreach (String alias in TeacherDomains)
+                if (Email.Contains(alias)) return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Returns true if Email has a student domain
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsStudentDomain(string Email)
+        {
+            foreach (String alias in StudentDomains)
+                if (Email.Contains(alias)) return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Returns true if Email belongs to UPV Member
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsUPVMemberDomain(string Email)
+        {
+            return IsStudentDomain(Email) || IsTeacherDomain(Email);
+        }
+    }
+}
