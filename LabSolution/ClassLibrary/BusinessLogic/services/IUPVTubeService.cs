ec13bc5ca1469e47ede39948f0520fca12a301d5
Patrick Haas <PHAAS@upv.edu.es>
2023-10-29 12:30:21 +0100

    #82 Function to add member to dbset implemented.

---
+++ LabSolution/ClassLibrary/BusinessLogic/services/IUPVTubeService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPVTube.BusinessLogic.Services
+{
+    public interface IUPVTubeService
+    {
+        void registerNewUser(string email, string name, DateTime date, string nick, string password);
+
+    }
+}
---
+++ LabSolution/ClassLibrary/BusinessLogic/services/UPVTubeService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UPVTube.Persistence;
+using UPVTube.BusinessLogic;
+using UPVTube.Entities;
+
+namespace UPVTube.BusinessLogic.Services
+{
+    public class UPVTubeService: IUPVTubeService
+    {
+
+        private readonly IDAL dal;
+
+        public UPVTubeService(IDAL dal) {
+            this.dal = dal;
+        }
+
+        public void removeAllData()
+        {
+            dal.Clear<Subject>();
+            dal.Clear<Member>();
+            dal.Clear<Content>();
+            dal.Clear<Visualization>();
+            dal.Clear<Comment>();
+            dal.Clear<Evaluation>();
+        }
+
+
+        public void registerNewUser(string email, string name, DateTime date, string nick, string password)
+        {
+            Member newMember = new Member(email, name, date, nick, password);
+
+            //Check if Member is not already in the system
+            if (dal.GetById<Member>(newMember.Nick) == null)
+            {
+                dal.Insert(newMember);
+                dal.Commit();
+            }
+
+            else throw new ServiceException("Person already exists.");
+        }
+
+
+    }
+}
