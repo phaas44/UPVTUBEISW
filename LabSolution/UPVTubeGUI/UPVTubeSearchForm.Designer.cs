fa067f2019b28c2723fef3add24447690dd4865f
Patrick Haas <PHAAS@upv.edu.es>
2024-01-10 20:30:42 +0100

    applied stash.

---
+++ LabSolution/UPVTubeGUI/UPVTubeSearchForm.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace UPVTubeGUI
+{
+    partial class UPVTubeSearchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.StartDateLabel = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.EndDateLabel = new System.Windows.Forms.Label();
+            this.NickLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.CoursesLabel = new System.Windows.Forms.Label();
+            this.CoursesCheckBoxList = new System.Windows.Forms.CheckedListBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StartDateLabel
+            // 
+            this.StartDateLabel.AutoSize = true;
+            this.StartDateLabel.Location = new System.Drawing.Point(28, 27);
+            this.StartDateLabel.Name = "StartDateLabel";
+            this.StartDateLabel.Size = new System.Drawing.Size(83, 20);
+            this.StartDateLabel.TabIndex = 0;
+            this.StartDateLabel.Text = "Start Date";
+            this.StartDateLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(32, 61);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(280, 26);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(357, 61);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(280, 26);
+            this.dateTimePicker2.TabIndex = 3;
+            // 
+            // EndDateLabel
+            // 
+            this.EndDateLabel.AutoSize = true;
+            this.EndDateLabel.Location = new System.Drawing.Point(353, 27);
+            this.EndDateLabel.Name = "EndDateLabel";
+            this.EndDateLabel.Size = new System.Drawing.Size(77, 20);
+            this.EndDateLabel.TabIndex = 2;
+            this.EndDateLabel.Text = "End Date";
+            // 
+            // NickLabel
+            // 
+            this.NickLabel.AutoSize = true;
+            this.NickLabel.Location = new System.Drawing.Point(721, 26);
+            this.NickLabel.Name = "NickLabel";
+            this.NickLabel.Size = new System.Drawing.Size(39, 20);
+            this.NickLabel.TabIndex = 4;
+            this.NickLabel.Text = "Nick";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(725, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(231, 26);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(32, 163);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(231, 26);
+            this.textBox2.TabIndex = 7;
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Location = new System.Drawing.Point(28, 128);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(38, 20);
+            this.TitleLabel.TabIndex = 6;
+            this.TitleLabel.Text = "Title";
+            // 
+            // CoursesLabel
+            // 
+            this.CoursesLabel.AutoSize = true;
+            this.CoursesLabel.Location = new System.Drawing.Point(353, 128);
+            this.CoursesLabel.Name = "CoursesLabel";
+            this.CoursesLabel.Size = new System.Drawing.Size(68, 20);
+            this.CoursesLabel.TabIndex = 8;
+            this.CoursesLabel.Text = "Courses";
+            // 
+            // CoursesCheckBoxList
+            // 
+            this.CoursesCheckBoxList.FormattingEnabled = true;
+            this.CoursesCheckBoxList.Location = new System.Drawing.Point(357, 163);
+            this.CoursesCheckBoxList.Name = "CoursesCheckBoxList";
+            this.CoursesCheckBoxList.Size = new System.Drawing.Size(171, 119);
+            this.CoursesCheckBoxList.TabIndex = 11;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(596, 163);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(141, 35);
+            this.SearchButton.TabIndex = 12;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 301);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1119, 376);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.CurrentChanged += new System.EventHandler(this.label1_Click);
+            // 
+            // UPVTubeSearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 689);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.CoursesCheckBoxList);
+            this.Controls.Add(this.CoursesLabel);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.TitleLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.NickLabel);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.EndDateLabel);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.StartDateLabel);
+            this.Name = "UPVTubeSearchForm";
+            this.Text = "UPVTubeSearchForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label StartDateLabel;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label EndDateLabel;
+        private System.Windows.Forms.Label NickLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.Label CoursesLabel;
+        private System.Windows.Forms.CheckedListBox CoursesCheckBoxList;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+    }
+}
